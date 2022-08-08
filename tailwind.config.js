cec64d2dca0521f6442a2c5285963a21ce317318
hassancodess <dev.hassancodess@gmail.com>
2022-08-09 00:47:25 +0500

    Finish Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
         sans: ['Josefin Sans', 'sans-serif'],
         alata: ['Alata'],
       },
+      backgroundImage: {
+        'hero-desktop': "url('/images/desktop/image-hero.jpg')",
+        'hero-mobile': "url('/images/mobile/image-hero.jpg')",
+      },
     },
   },
   plugins: [],
