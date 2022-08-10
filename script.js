18640dff0a2c1fa301b33c298ee79d8980f99f99
hassancodess <dev.hassancodes@gmail.com>
2022-08-10 13:28:35 +0500

    fix bug

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ btn.addEventListener('click', navToggle)
 
 function navToggle() {
   btn.classList.toggle('open')
-  menu.classList.toggle('flex ')
+  menu.classList.toggle('flex')
   menu.classList.toggle('hidden')
 }
